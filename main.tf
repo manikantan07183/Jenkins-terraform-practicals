b56cbb20fe0b0e56353eb764ce00e4eb9028acab
manikantan07183 <manikantanvasanth7@gmail.com>
2025-04-04 16:04:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+# Create IAM User
+resource "aws_iam_user" "new_user" {
+  name = "terraform-user"
+  force_destroy = true
+}
+
+# Create EC2 Instance
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-00a929b66ed6e0de6"  # Amazon Linux 
+  instance_type = "t2.micro"
+  force_destroy = true
+
+}
